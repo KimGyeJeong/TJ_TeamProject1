98d4962adb455b32976e7f172cfc3a386c7e2376
Queen_Lux <pmwkdo@gmail.com>
2022-07-14 15:31:40 +0900

    계정 dao push

---
+++ TJ_TeamProject1/src/main/java/team/project/dao/GyeJeongDAO.java
@@ -0,0 +1,72 @@
+package team.project.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class GyeJeongDAO {
+	int result = -1;
+	String sql = "";
+
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+
+	private Connection getConnection() throws SQLException, NamingException {
+		Context ctx = new InitialContext();
+		Context env = (Context) ctx.lookup("java:comp/env");
+		DataSource ds = (DataSource) env.lookup("jdbc/orcl");
+
+		return ds.getConnection();
+	}
+
+	private void closeConnection(PreparedStatement pstmt, Connection conn) {
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void closeConnection(ResultSet rs, PreparedStatement pstmt, Connection conn) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	// new method here
+
+
+}
