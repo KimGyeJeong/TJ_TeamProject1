e938d699c75bdce84e391e044a0d5d5f19402018
tjoeun <tjoeun@DESKTOP-B6BR8HC>
2022-07-14 12:52:04 +0900

    이민형dto

---
+++ TJ_TeamProject1/src/main/java/team/project/model/CategoryDTO.java
@@ -0,0 +1,21 @@
+package team.project.model;
+
+public class CategoryDTO {
+	private int ca_no;
+	private String ca_name;
+	public int getCa_no() {
+		return ca_no;
+	}
+	public void setCa_no(int ca_no) {
+		this.ca_no = ca_no;
+	}
+	public String getCa_name() {
+		return ca_name;
+	}
+	public void setCa_name(String ca_name) {
+		this.ca_name = ca_name;
+	}
+	
+	
+	
+}
