a1fdb401c4a5fac6e438e0b6d6cab8ab1177a92f
kimGiGong <kim961234@daum.net>
2022-07-14 12:57:27 +0900

    dto 6-10

---
+++ TJ_TeamProject1/src/main/java/team/project/model/WishListDTO.java
@@ -0,0 +1,22 @@
+package team.project.model;
+
+public class WishListDTO {
+	
+	String user_id;
+    int p_no;
+    
+    
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public int getP_no() {
+		return p_no;
+	}
+	public void setP_no(int p_no) {
+		this.p_no = p_no;
+	}
+    
+}
